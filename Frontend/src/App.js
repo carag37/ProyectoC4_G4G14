ab4c4161df6a030abc1c984f78cb542b65447a7a
carag37 <carag37@gmail.com>
2022-11-29 11:25:15 -0500

    Avance Front 20221129_1124

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "./components/Login";
+import CreateSubject from "./components/createSubject";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element = {<Login/>}/>
+        <Route path="/crear-materia" exact element = {<CreateSubject/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ Frontend/src/components/CreateSubject.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CreateSubject = () => {
+return (
+<div>
+<h1> Crear Materia</h1>
+<h2> Ingrese información solicitada</h2>
+<input placeholder = "Nombre" />
+<input placeholder = "Horario" />
+<textarea placeholder = "Descripción" />
+
+<button>Crear Cuenta</button>
+
+</div>
+
+
+)
+
+}
+export default CreateSubject;
