75989f9b378838f48cdeaac0721fd22c19c5b5ba
carag37 <carag37@gmail.com>
2022-12-11 14:16:13 -0500

    Se adicionan validaciones a Materias, Cursos y Boletines para evitar duplicaciones y adicionar elementos a los Array sin duplicidad

--- BackG14/controllers/boletinController.js
+++ BackG14/controllers/boletinController.js
@@ -1,19 +1,34 @@
 import BoletinSchema from "../models/boletinModels.js";
+import AlumnoSchema from "../models/alumnoModels.js";
+import MateriaSchema from "../models/materiaModels.js";
 
 //--------------------------CREATE--------------------------------------------
 
 async function crearBoletin(req, res) {
 
-    const {materia, notas, alumno, observaciones } = req.body;
+    const { materia, notas, alumno, observaciones } = req.body
     let docBoletin;
+    let alumnoV = await AlumnoSchema.find({"_id": alumno}) 
+    let materiaV = await MateriaSchema.find({"_id": materia})
+
+    if (alumnoV.length==0 || materiaV==0){ return res.status(400).json({ msg: "La materia o el alumno no existen"}); }
+    
+    const boletin = await BoletinSchema.find({ 
+        "materia": materia,
+        "alumno": alumno
+    });
+    
+    if (boletin.length!=0) {        
+        return res.status(400).json({ msg: "El boletin ya existe para el alumno " + alumnoV[0].nombre +  " con id " + alumnoV[0].idAlumno + " para la materia " + materiaV[0].nombre});
+    }
 
     try {
         docBoletin = await BoletinSchema.create({
-                    
+
             "materia": materia,
-            "notas":notas,
-            "alumno":alumno,
-            "observaciones":observaciones
+            "notas": notas,
+            "alumno": alumno,
+            "observaciones": observaciones
 
         })
     } catch (error) {
@@ -30,13 +45,23 @@ async function crearBoletin(req, res) {
 
 async function leerBoletin(req, res) {
 
-    const {_id} = req.body
-    let docBoletin;
+    const {nombreMateria, idAlumno} = req.body
     
+    let docBoletin
+    let alumnoV = await AlumnoSchema.find({"idAlumno": idAlumno}) 
+    let materiaV = await MateriaSchema.find({"nombre": nombreMateria})
+
+    if (alumnoV.length==0 || materiaV==0){ return res.status(400).json({ msg: "La materia o el alumno no existen"}); }
+
+    let materia = materiaV[0]._id;
+    let alumno = alumnoV[0]._id
+
     try {
-        
+
         docBoletin = await BoletinSchema.find({
-            "_id": _id,
+            "materia": materia,
+            "alumno": alumno
+
         })
 
     } catch (error) {
@@ -53,14 +78,23 @@ async function leerBoletin(req, res) {
 
 async function actualizarBoletin(req, res) {
 
-    const { _id, cambios } = req.body;
-    let docBoletin;
+    const {nombreMateria, idAlumno, cambios} = req.body
+    
+    let docBoletin
+    let alumnoV = await AlumnoSchema.find({"idAlumno": idAlumno}) 
+    let materiaV = await MateriaSchema.find({"nombre": nombreMateria})
+
+    if (alumnoV.length==0 || materiaV==0){ return res.status(400).json({ msg: "La materia o el alumno no existen"}); }
+
+    let materia = materiaV[0]._id;
+    let alumno = alumnoV[0]._id
 
     try {
-        docBoletin = await BoletinSchema.updateOne({  //el updateone busca y edita un valor que debe ser único de elementos definidos en el modelo
-            "_id": _id  //Lo que está entre comillas se debe llamar igual al parámetro en la DB.
-        }, cambios, { runValidators: true })//{"edad":123})  //Primer parámetro para buscar, segundo parámetro para editar (objeto Json). //Método 2
-
+        docBoletin = await BoletinSchema.find({  //el updateone busca y edita un valor que debe ser único de elementos definidos en el modelo
+            "materia": materia,
+            "alumno": alumno  //Lo que está entre comillas se debe llamar igual al parámetro en la DB.
+        })//{"edad":123})  //Primer parámetro para buscar, segundo parámetro para editar (objeto Json). //Método 2
+   
     } catch (error) {
         res.status(400)
         res.json(error.message);
@@ -69,11 +103,29 @@ async function actualizarBoletin(req, res) {
     }
 
 
-    if (docBoletin.matchedCount == 0) {
-        return res.status(400).json({ msg: "El boletin " + _id + " no ha sido encontrado" });
+    if (docBoletin.length == 0) {
+        return res.status(400).json({ msg: "El boletin no existe para el alumno " + alumnoV[0].nombre +  " con id " + alumnoV[0].idAlumno + " para la materia " + materiaV[0].nombre });
     }
+
+    if (cambios==null){return res.status(200).json({ msg: "No se solicitaron cambios"});}
+
+    docBoletin[0].materia = cambios.materia || docBoletin[0].materia
+    docBoletin[0].alumno = cambios.alumno || docBoletin[0].alumno
+    docBoletin[0].observaciones = cambios.observaciones || docBoletin[0].observaciones
+
+   
+
+    if (docBoletin[0].notas.length != 0 && cambios.notas != null){
     
-    else { res.status(200).json({ msg: "El boletin " + _id + " modificado correctamente" }) }
+    for (let i = 0; i<cambios.notas.length; i++){ 
+        let nota=cambios.notas[i]
+        docBoletin[0].notas.push(nota)
+    }
+    }
+
+
+    docBoletin[0].save();
+    res.status(200).json({ docBoletin })
 
 }
 
--- BackG14/controllers/cursoController.js
+++ BackG14/controllers/cursoController.js
@@ -1,22 +1,33 @@
 import CursoSchema from "../models/cursoModels.js";
+import MateriaSchema from "../models/materiaModels.js";
 
 //--------------------------CREATE--------------------------------------------
 
 async function crearCurso(req, res) {
 
     const { descripcion, materia } = req.body;
-    
+    let materiaV = [];
     let docCurso;
+
+    if (materia != null) {
+
+        for (let i = 0; i < materia.length; i++) {
+            try {
+                materiaV[i] = await MateriaSchema.find({ "_id": materia[i] })
+            } catch (error) { return res.status(400).json({ msg: "La materia " + materia[i] + " no existe" }) }
+        }
+    }
 
     try {
         docCurso = await CursoSchema.create({
-                    
+
             "descripcion": descripcion,
             "materia": materia,
 
         })
     } catch (error) {
         res.status(400)
+        if (error.code == 11000) { return res.json({ msg: "El curso " + descripcion + " ya existe" }) }
         res.json(error.message);
         return  //return para evitar enviar 2 respuestas por ejecución
     }
@@ -31,9 +42,10 @@ async function leerCurso(req, res) {
 
     const { descripcion } = req.body
     let docCurso;
-    console.log(descripcion)
+
+
     try {
-        
+
         docCurso = await CursoSchema.find({
             "descripcion": descripcion,
         })
@@ -44,6 +56,8 @@ async function leerCurso(req, res) {
         return  //return para evitar enviar 2 respuestas por ejecución
     }
 
+    if (docCurso.length == 0) { return res.status(400).json({ msg: "El curso no existe" }); }
+
     res.status(200);  //código de Ok, si es SendStatus no hace más consultas.
     res.json(docCurso); //envío el objeto creado como un JSON
 }
@@ -53,27 +67,64 @@ async function leerCurso(req, res) {
 async function actualizarCurso(req, res) {
 
     const { descripcion, cambios } = req.body;
-    let docCurso;
 
-    try {
-        docCurso = await CursoSchema.updateOne({  //el updateone busca y edita un valor que debe ser único de elementos definidos en el modelo
-            "descripcion": descripcion  //Lo que está entre comillas se debe llamar igual al parámetro en la DB.
-        }, cambios, { runValidators: true })//{"edad":123})  //Primer parámetro para buscar, segundo parámetro para editar (objeto Json). //Método 2
+    let docCurso = await CursoSchema.find({ "descripcion": descripcion })
 
-    } catch (error) {
-        res.status(400)
-        res.json(error.message);
+    if (docCurso.length == 0) { return res.status(400).json({ msg: "El curso " + descripcion + " no existe" }); }
 
-        return  //return para evitar enviar 2 respuestas por ejecución
+    if (cambios == null) { return res.status(200).json({ msg: "No se solicitaron cambios" }); }
+
+    docCurso[0].descripcion = cambios.descripcion || docCurso[0].descripcion  //en caso de que exista se actualiza la descripción
+
+    let materiaV = [];
+
+    if (cambios.materia != null) {
+        for (let i = 0; i < cambios.materia.length; i++) {
+            try {
+                materiaV[i] = await MateriaSchema.find({ "_id": cambios.materia[i] })
+
+            } catch (error) { return res.status(400).json({ msg: "La materia " + cambios.materia[i] + " no existe" }) }
+        
+        if(materiaV[i].length==0){return res.status(400).json({ msg: "La materia " + cambios.materia[i] + " no existe" })}
+        }
+
+        
+
+        let materia = []
+        let contador = 0
+        let mensaje = ""
+
+
+        for (let i = 0; i < cambios.materia.length; i++) {
+            for (let j = 0; j < docCurso[0].materia.length; j++) {
+                //console.log(cambios.materia[i])
+                //console.log(docCurso[0].materia[j].toString())
+                if (cambios.materia[i].toString() == docCurso[0].materia[j].toString()) { contador = contador + 1; }
+                //console.log(contador)
+            }
+
+            if (contador == 0) {
+                materia.push(cambios.materia[i])
+            } else {
+                mensaje = mensaje + cambios.materia[i] + " ";
+                contador = 0
+            }
+            
+        }
+
+        console.log(mensaje + " ya en curso")
+
+        for (let i = 0; i < materia.length; i++) {
+            let mats = materia[i]
+            docCurso[0].materia.push(mats)
+        }
+
+       
+
     }
 
-
-    if (docCurso.matchedCount == 0) {
-        return res.status(400).json({ msg: "El curso " + descripcion + " no ha sido encontrado" });
-    }
-    
-    else { res.status(200).json({ msg: "El curso " + descripcion + " modificado correctamente" }) }
-
+    docCurso[0].save();
+    res.status(200).json({ docCurso })
 }
 
 //-------------------------DELETE-------------------------------------------
