e367ee624330eab0d961579b99d3106dd32270c6
anadiazmt <113481271+anadiazmt@users.noreply.github.com>
2022-12-14 11:35:23 -0500

    Conexión con la base de datos a través de Visual

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import materiaRouter from "./routes/materiaRouter.js";
 import calificacionRouter from "./routes/calificacionRouter.js";
@@ -17,6 +18,13 @@ app.listen(port, ()=>{
     console.log("El servidor está ejecutandose correctamente en el puerto:" +port);
 });
 
+mongoose.connect("mongodb+srv://calificAPP:rootcalificAPP@clustercalificapp.c6gvwmq.mongodb.net/test", (err)=>{
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Se ha conectado a la base de datos");
+    }
+})
 app.use(express.json())
 app.use("/api/usuarios", userRouter)
 app.use("/api/materias", materiaRouter)
