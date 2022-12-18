8e54fb13d5c206aacb150b3d0ce921aa811ec3cc
Sandra Esquivel <sandra.esquivel.mt@usa.edu.co>
2022-12-18 15:44:34 -0500

    Cambios en App.js

--- calificapp/src/App.js
+++ calificapp/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Admin from './components/Admin';
-import Home from './components/Home';
-import CrearCuenta from "./components/CrearCuenta";
-import CrearAdmin from './components/CrearAdmin';
-import CrearAcudiente from './components/CrearAcudiente';
-import CrearAlumno from './components/CrearAlumno';
+
+//Principal
+import Login from './components/Login.js';
+import Admin from './components/Admin.js';
+import Home from './components/Home.js';
+import CrearInicio  from  './components/CrearInicio'
+
+//Usuario
+import CrearCuentaIn from "./components/usuario/CrearCuentaIn";
+import HomeUsuario from "./components/usuario/HomeUsuario.js";
+import MenuUsuarios from './components/usuario/MenuUsuarios.js'
+import ActualizarUsuario from './components/usuario/ActualizarUsuario.js';
+
+//Administrador
+import CrearAdmin from './components/Admin/CrearAdmin.js';
+import ActualizarAdmin from './components/Admin/ActualizarAdmin.js';
+import ConsultarAdmin from './components/Admin/ConsultarAdmin.js';
+import HomeAdmin from './components/Admin/HomeAdmin.js';
+
+//Acudiente
+import CrearAcudiente from './components/Acudiente/CrearAcudiente.js';
+import HomeAcudiente from './components/Acudiente/HomeAcudiente.js'
 
 //Alumno
 import CrearAlumno from './components/Alumno/CrearAlumno.js';
@@ -35,26 +50,47 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" exact element={<Home />} />
-                <Route path="/login" exact element={<Login />} />
-                <Route path="/admin" exact element={<Admin />} />
-                <Route path="/crear-cuenta" exact element={<CrearCuenta />} />
-                <Route path="/crear-cuenta-in" exact element={<CrearCuentaIn />} />
-                <Route path="/crear-admin" exact element={<CrearAdmin />} />
-                <Route path="/crear-acudiente" exact element={<CrearAcudiente />} />
-                <Route path="/crear-alumno" exact element={<CrearAlumno />} />
-                <Route path="/menu-materia" exact element={<MenuMateria />} />
-                <Route path="/crear-materia" exact element={<CrearMateria />} />
-                <Route path="/ver-materia" exact element={<VerMateria />} />
-                <Route path="/actualizar-materia" exact element={<ActualizarMateria />} />
-                <Route path="/menu-curso" exact element={<MenuCurso />} />
-                <Route path="/crear-curso" exact element={<CrearCurso />} />
-                <Route path="/ver-curso" exact element={<VerCurso />} />
-                <Route path="/actualizar-curso" exact element={<ActualizarCurso />} />
-                <Route path="/menu-boletin" exact element={<MenuBoletin />} />
-                <Route path="/crear-boletin" exact element={<CrearBoletin />} />
-                <Route path="/ver-boletin" exact element={<VerBoletin />} />
-                <Route path="/actualizar-boletin" exact element={<ActualizarBoletin />} />
+                
+                <Route path="/" exact element= {<Home/>} />
+                <Route path="/login" exact element= {<Login/>} />
+                <Route path="/admin" exact element= {<Admin/>} />
+                <Route path="/crear-inicio" exact element= {<CrearInicio/>} />
+
+                <Route path="/menu-usuarios" exact element={<MenuUsuarios/>} />
+                <Route path="/crear-cuentaIn" exact element={<CrearCuentaIn/>} />
+                <Route path="/home-usuario" exact element={<HomeUsuario/>} />
+                <Route path="/actualizar-usuario/:idUsuario" exact element= {<ActualizarUsuario/>} />
+                <Route path="/actualizar-usuario/:tipoUsuario" exact element= {<ActualizarUsuario/>} />
+
+                <Route path="/crear-admin" exact element={<CrearAdmin />} />   
+                <Route path="/crear-admin/:usuarioSistema" exact element= {<CrearAdmin/>} />
+                <Route path="/actualizar-admin/:usuarioSistema" exact element= {<ActualizarAdmin/>} />
+                <Route path="/consultar-admin/:usuarioSistema" exact element= {<ConsultarAdmin/>} />
+                <Route path="/home-admin" exact element= {<HomeAdmin/>} />
+                
+                
+                <Route path="/home-acudiente" exact element= {<HomeAcudiente/>} />
+                <Route path="/crear-acudiente" exact element={<CrearAcudiente/>} />
+
+                
+                <Route path="/crear-alumno" exact element={<CrearAlumno/>} />
+                <Route path="/home-alumno" exact element={<HomeAlumno/>} />
+                <Route path="/actualizar-alumno/:id" exact element={<ActualizarAlumno/>} />
+                
+                <Route path="/menu-materia" exact element={<MenuMateria/>} />
+                <Route path="/crear-materia" exact element={<CrearMateria/>} />
+                <Route path="/ver-materia" exact element={<VerMateria/>} />
+                <Route path="/actualizar-materia" exact element={<ActualizarMateria/>} />
+
+                <Route path="/menu-curso" exact element={<MenuCurso/>} />
+                <Route path="/crear-curso" exact element={<CrearCurso/>} />
+                <Route path="/ver-curso" exact element={<VerCurso/>} />
+                <Route path="/actualizar-curso" exact element={<ActualizarCurso/>} />
+
+                <Route path="/menu-boletin" exact element={<MenuBoletin/>} />
+                <Route path="/crear-boletin" exact element={<CrearBoletin/>} />
+                <Route path="/ver-boletin" exact element={<VerBoletin/>} />
+                <Route path="/actualizar-boletin" exact element={<ActualizarBoletin/>} />
             </Routes>
         </Router>
 
